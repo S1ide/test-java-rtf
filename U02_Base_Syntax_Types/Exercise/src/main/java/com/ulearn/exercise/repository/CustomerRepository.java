026a0e4bbd1f1f518bab29fe9c55395ed22ee8f3
Kopotev Nikita <122271491+viaarm@users.noreply.github.com>
2023-02-20 17:50:42 +0500

    test

--- U02_Base_Syntax_Types/Exercise/src/main/java/com/ulearn/exercise/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ulearn.exercise.repository;
-
-import com.ulearn.exercise.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
