87a6ec5378f422fd1fd7ff42715ccff62b118a99
Vitaliy <vitaliy-efimov111@mail.ru>
2023-11-15 16:32:15 +0500

    Update `theme 2`

--- U02_Database/Exercise/src/main/java/ulearn/practice2/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package ulearn.practice2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ulearn.practice2.model.Customer;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
