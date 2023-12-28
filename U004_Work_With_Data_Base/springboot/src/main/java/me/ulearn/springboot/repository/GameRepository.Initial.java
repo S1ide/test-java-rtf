65f65ce162a96b24791597806ec36100c6bb1719
Vitaliy <vitaliy-efimov111@mail.ru>
2023-12-28 23:51:44 +0500

    Update course

---
+++ U004_Work_With_Data_Base/springboot/src/main/java/me/ulearn/springboot/repository/GameRepository.Initial.java
@@ -0,0 +1,10 @@
+package me.ulearn.springboot.repository;
+
+import me.ulearn.springboot.entity.Game;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+//#region Task
+public interface GameRepository {
+}
+//#endregion Task
---
+++ U005_Configuring_Spring_Boot_App/springboot2/src/main/java/me/ulearn/springboot/controller/FilmController.Initial.java
@@ -0,0 +1,12 @@
+package me.ulearn.springboot.controller;
+
+import me.ulearn.springboot.config.Film;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//#region Task
+public class FilmController {
+
+}
+//#endregion Task
